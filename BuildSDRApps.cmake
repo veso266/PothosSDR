2fa48b0c41547246807cce3384effa3e84bf1a8b
veso266 <veso266@gmail.com>
2022-04-27 00:54:42 +0200

    Removed CubicSDR as I have trouble building SoapySDR

--- BuildSDRApps.cmake
+++ BuildSDRApps.cmake
@@ -9,7 +9,7 @@
 ############################################################
 
 set(CUBIC_SDR_BRANCH master)
-set(INSPECTRUM_SDR_BRANCH master)
+set(INSPECTRUM_SDR_BRANCH main)
 
 ############################################################
 ## Build CubicSDR
@@ -17,29 +17,29 @@ set(INSPECTRUM_SDR_BRANCH master)
 ## -DWX_ROOT_DIR is a hack to prevent FindwxWidgets.cmake
 ## from clearing wxWidgets_LIB_DIR the first configuration.
 ############################################################
-MyExternalProject_Add(CubicSDR
-    DEPENDS SoapySDR wxWidgets liquiddsp fftw
-    GIT_REPOSITORY https://github.com/cjcliffe/CubicSDR.git
-    GIT_TAG ${CUBIC_SDR_BRANCH}
-    CMAKE_DEFAULTS ON
-    CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-        -DWX_ROOT_DIR=${wxWidgets_ROOT_DIR}
-        -DwxWidgets_ROOT_DIR=${wxWidgets_ROOT_DIR}
-        -DwxWidgets_LIB_DIR=${wxWidgets_LIB_DIR}
-        -DFFTW_INCLUDES=${FFTW3F_INCLUDE_DIRS}
-        -DFFTW_LIBRARIES=${FFTW3F_LIBRARIES}
-        -DFFTW_DLL=${FFTW3F_LIBRARIES} #this gets installed to bin
-        -DLIQUID_INCLUDES=${LIQUIDDSP_INCLUDE_DIR}
-        -DLIQUID_LIBRARIES=${LIQUIDDSP_LIBRARY}
-        -DLIQUID_DLL=${LIQUIDDSP_DLL}
-        -DSoapySDR_DIR=${CMAKE_INSTALL_PREFIX}
-    LICENSE_FILES LICENSE
-)
+#MyExternalProject_Add(CubicSDR
+#    DEPENDS SoapySDR wxWidgets liquiddsp fftw
+#    GIT_REPOSITORY https://github.com/cjcliffe/CubicSDR.git
+#    GIT_TAG ${CUBIC_SDR_BRANCH}
+#    CMAKE_DEFAULTS ON
+#    CMAKE_ARGS
+#        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+#        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+#        -DWX_ROOT_DIR=${wxWidgets_ROOT_DIR}
+#        -DwxWidgets_ROOT_DIR=${wxWidgets_ROOT_DIR}
+#        -DwxWidgets_LIB_DIR=${wxWidgets_LIB_DIR}
+#        -DFFTW_INCLUDES=${FFTW3F_INCLUDE_DIRS}
+#        -DFFTW_LIBRARIES=${FFTW3F_LIBRARIES}
+#        -DFFTW_DLL=${FFTW3F_LIBRARIES} #this gets installed to bin
+#        -DLIQUID_INCLUDES=${LIQUIDDSP_INCLUDE_DIR}
+#        -DLIQUID_LIBRARIES=${LIQUIDDSP_LIBRARY}
+#        -DLIQUID_DLL=${LIQUIDDSP_DLL}
+#        -DSoapySDR_DIR=${CMAKE_INSTALL_PREFIX}
+#    LICENSE_FILES LICENSE
+#)
 
-list(APPEND CPACK_PACKAGE_EXECUTABLES "CubicSDR" "CubicSDR")
-list(APPEND CPACK_CREATE_DESKTOP_LINKS "CubicSDR")
+#list(APPEND CPACK_PACKAGE_EXECUTABLES "CubicSDR" "CubicSDR")
+#list(APPEND CPACK_CREATE_DESKTOP_LINKS "CubicSDR")
 
 ############################################################
 ## Build Inspectrum
